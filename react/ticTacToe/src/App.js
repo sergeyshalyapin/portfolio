fc90a1549160881a6dd747829152d6e32a73f845
sergeyshalyapin <sergey.shalyapin.90@gmail.com>
2025-07-11 18:06:35 +0200

    initial commit for tic-tac-toe project

---
+++ react/ticTacToe/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Tic Tac Toe!</h1>
+      <p>Start playing by selecting a cell.</p>
+    </div>
+  );
+}
+
+export default App;
